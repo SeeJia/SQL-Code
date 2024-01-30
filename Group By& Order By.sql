e55261c644d239ddff441bb33f9b4aa7d17dccda
See Jia Jia <88718980+SeeJia@users.noreply.github.com>
2024-01-30 11:46:50 +0800

    Add files via upload

---
+++ Group By& Order By.sql
@@ -0,0 +1,10 @@
+--GROUP BY, ORDER BY
+
+SELECT FirstName FROM EmployeeDemographics
+GROUP BY FirstName  
+
+SELECT * FROM EmployeeDemographics
+ORDER BY Age
+
+SELECT * FROM EmployeeDemographics 
+ORDER BY Age DESC
