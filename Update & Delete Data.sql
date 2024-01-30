eeed5b2a4186d949c1fcda72c2053065c6cd2804
See Jia Jia <88718980+SeeJia@users.noreply.github.com>
2024-01-30 20:16:39 +0800

    Add files via upload

---
+++ Update & Delete Data.sql
@@ -0,0 +1,4 @@
+UPDATE EmployeeDemographics SET EmployeeID = 1012, Age = 31, Gender = 'Female'
+WHERE FirstName = 'Holly' AND LastName = 'Flax' 
+
+DELETE FROM EmployeeDemographics WHERE EmployeeID = 1005
