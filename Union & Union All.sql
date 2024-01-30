df2ab810924c69ca0d14b970817eb7c120a17d2c
See Jia Jia <88718980+SeeJia@users.noreply.github.com>
2024-01-30 18:17:52 +0800

    Add files via upload

---
+++ Union & Union All.sql
@@ -0,0 +1,10 @@
+--UNION & UNION ALL
+
+SELECT * FROM EmployeeDemographics
+UNION
+SELECT * FROM WareHouseEmployeeDemographics 
+
+SELECT * FROM EmployeeDemographics
+UNION ALL
+SELECT * FROM WareHouseEmployeeDemographics 
+  
